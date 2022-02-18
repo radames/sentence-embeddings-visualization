021ceab7f16f74f0edfc0fc8dc9bee6a95def43f
Radames Ajna <radamajna@gmail.com>
2022-02-17 23:26:16 -0800

    update comment, disable warning msg

--- embeddings_encoder.py
+++ embeddings_encoder.py
@@ -1,8 +1,9 @@
-# from https://huggingface.co/sentence-transformers/multi-qa-MiniLM-L6-cos-v1
+# from https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
 from transformers import AutoTokenizer, AutoModel
 import torch
 import torch.nn.functional as F
-
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 class EmbeddingsEncoder:
     def __init__(self):
@@ -17,7 +18,8 @@ class EmbeddingsEncoder:
     def mean_pooling(self, model_output, attention_mask):
         # First element of model_output contains all token embeddings
         token_embeddings = model_output.last_hidden_state
-        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        input_mask_expanded = attention_mask.unsqueeze(
+            -1).expand(token_embeddings.size()).float()
         return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
     # Encode text
